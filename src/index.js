e84322656bb47c33200f6886cffdfbf2d6fc52ce
Kayla Hunt <huntkay1@msu.edu>
2024-04-17 18:17:43 -0400

    Style logo and nav

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+import todayIcon from './today-icon.png';
+import melon from './melon-icon.png';
+
+const header = document.querySelector("header");
+const melonIcon = new Image(58, 58);
+melonIcon.src = melon;
+melonIcon.classList.add("melon-icon");
+
+header.appendChild(melonIcon);
+
+
+// const todayImg = new Image(45, 45);
+// todayImg.src = todayIcon;
+
+
+// let nav1 = document.getElementById("nav-1");
+// nav1.appendChild(todayImg);
+
+
+//its there but its the same color as the background
