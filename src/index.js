89cc2d5630bde3acd95b5c19a07a7367e8c02c42
Kayla Hunt <huntkay1@msu.edu>
2024-04-18 19:46:36 -0400

    Format fist nav item

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import './style.css';
-import todayIcon from './today-icon.png';
+import todayIconLight from './today-icon-light.png';
+import weekIconLight from './week-icon-light.png';
+import monthIconLight from './month-icon-light.png';
+import allIconLight from './all-icon-light.png';
 import melon from './melon-icon.png';
 
+//add logo icon
 const header = document.querySelector("header");
-const melonIcon = new Image(58, 58);
+const melonIcon = new Image(55, 55);
 melonIcon.src = melon;
 melonIcon.classList.add("melon-icon");
-
 header.appendChild(melonIcon);
+
+const nav1 = document.getElementById("nav-1");
+const listItem = document.createElement('li');
+const navItemName = document.createElement('p');
+navItemName.innerHTML = "Today";
+listItem.classList.add("nav1-item");
+const todayIcon = new Image(35, 35);
+todayIcon.src = todayIconLight;
+
+listItem.appendChild(todayIcon);
+listItem.appendChild(navItemName);
+nav1.appendChild(listItem);
+
 
 
 // const todayImg = new Image(45, 45);
-// todayImg.src = todayIcon;
+// todayImg.src = todayIcon;\
 
 
 // let nav1 = document.getElementById("nav-1");
