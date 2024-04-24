27a48178f203ce4cf657846bf4bb8cb6aec5afac
Kayla Hunt <huntkay1@msu.edu>
2024-04-24 18:43:48 -0400

    Style active tab

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import monthIconLight from './month-icon-light.png';
 import allIconLight from './all-icon-light.png';
 import melon from './melon-icon.png';
 
+
 //icons
 const melonIcon = new Image(55, 55);
 melonIcon.src = melon;
@@ -18,6 +19,7 @@ weekIcon.src = weekIconLight;
 const allIcon = new Image(35,35);
 allIcon.src = allIconLight;
 
+
 //add logo icon
 const logo = document.getElementById("logo");
 melonIcon.classList.add("melon-icon");
@@ -28,31 +30,37 @@ logo.appendChild(melonIcon);
 const nav1 = document.getElementById("nav-1");
 
 const navItems = {
-    Today : todayIcon,
+    "Today" : todayIcon,
     "This Week" : weekIcon,
     "This Month" : monthIcon,
     "All Tasks" : allIcon
 }
 
-// Object.entries(navItems).forEach(([navTitle, icon]) => {
-//     const listItem = document.createElement('li');
-//         const navItemTitle = document.createElement('p');
-//         navItemTitle.innerHTML = navTitle;
-//         listItem.classList.add("nav1-item");
-//         listItem.classList.add("nav1-item");
-//         listItem.appendChild(icon);
-//         listItem.appendChild(navItemTitle);
-//         nav1.appendChild(listItem);
-// })
-
-
 Object.entries(navItems).forEach(([navTitle, icon]) => {
     const listItem = document.createElement('button');
         const navItemTitle = document.createElement('p');
         navItemTitle.innerHTML = navTitle;
-        listItem.classList.add("nav1-item");
-        listItem.classList.add("nav1-item");
         listItem.appendChild(icon);
         listItem.appendChild(navItemTitle);
         nav1.appendChild(listItem);
 })
+
+const buttons = [...document.querySelectorAll('button')];
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        changeTab(e)
+    });
+})
+
+const changeTab = (e) => {
+
+    buttons.forEach(button => {
+        if (button.classList.contains("active")) {
+            button.classList.remove("active");
+        }
+    })
+    var activeTab = e.target;
+
+    activeTab.classList.add("active");
+}
