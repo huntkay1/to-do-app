ff44ad1e4593336dd941b2c28a50d289b211bfa1
Kayla Hunt <huntkay1@msu.edu>
2024-05-06 18:08:36 -0400

    Update proj list item style

--- src/add-project.js
+++ src/add-project.js
@@ -18,23 +18,27 @@ function addProjectNametoArray() {
     projectNames.push(projectNameInput.value);
     projectNameInput.value = "";
     updateProjectListUI();
-
 }
 
 function updateProjectListUI() {
     projectNames.map(projName => {
         const projectListItem = document.createElement('li');
         const projectBttn = document.createElement('button');
-        const bttnName = document.createElement('p');
+        const bttnName = document.createElement('div');
+        bttnName.classList.add("button-name");
+        const icon = document.createElement('div');
+        icon.classList.add("proj-list-icon");
 
         bttnName.innerHTML = projName;
+        icon.innerHTML = '<svg fill="#f0fff0" width="25px" height="25px" viewBox="0 0 24 24" version="1.2" baseProfile="tiny" xmlns="http://www.w3.org/2000/svg"><path d="M13.293 7.293c-.391.391-.391 1.023 0 1.414l2.293 2.293h-7.586c-.552 0-1 .448-1 1s.448 1 1 1h7.586l-2.293 2.293c-.391.391-.391 1.023 0 1.414.195.195.451.293.707.293s.512-.098.707-.293l4.707-4.707-4.707-4.707c-.391-.391-1.023-.391-1.414 0z"/></svg>';
         projectBttn.appendChild(bttnName);
+        projectBttn.appendChild(icon);
         projectListItem.appendChild(projectBttn);
         projectBttn.classList.add("nav2-item");
         nav2.appendChild(projectListItem);
-    }
-    )
+    });
 }
+
 
 
 
