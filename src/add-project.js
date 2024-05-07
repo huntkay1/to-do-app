a3e92aea22f4fec6722084624e0f8f0ed577e106
Kayla Hunt <huntkay1@msu.edu>
2024-05-07 17:43:35 -0400

    Update select options

--- src/add-project.js
+++ src/add-project.js
@@ -46,6 +46,8 @@ function updateProjectListUI() {
     });
 }
 
+export { projectNames }
+
 
 
 
--- src/add-task.js
+++ src/add-task.js
@@ -1,11 +1,26 @@
+import { projectNames } from './add-project.js'
+
+
 const addTaskBttn = document.getElementById("add-task");
 const addTaskDialog = document.getElementById("add-task-popup");
 const dialogCloseBttn = document.getElementById("close");
+const selectInput = document.getElementById("form-select");
+
 
 addTaskBttn.addEventListener('click', () => {
     addTaskDialog.showModal();
+    updateSelectOptions();
 })
 dialogCloseBttn.addEventListener('click', () => {
     addTaskDialog.close();
     document.querySelector('form').reset();
 })
+
+function updateSelectOptions() {
+    projectNames.forEach(projName => {
+        const option = document.createElement('option');
+        option.innerHTML = projName;
+        selectInput.appendChild(option);
+    })
+}
+
