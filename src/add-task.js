c48ac7f76d03e9ca99e052aece51133e293d7c25
Kayla Hunt <huntkay1@msu.edu>
2024-05-08 20:38:57 -0400

    Create localstorage module
    This module checks if local storage is available
    and if so, adds the task object to an array which
    is then saved in local storage.

--- src/add-task.js
+++ src/add-task.js
@@ -1,5 +1,5 @@
 import { projectNames } from './add-project.js'
-
+import { addToLocalStorage } from './local-storage.js';
 
 const addTaskBttn = document.getElementById("add-task");
 const addTaskDialog = document.getElementById("add-task-popup");
@@ -7,7 +7,7 @@ const dialogCloseBttn = document.getElementById("close");
 const selectInput = document.getElementById("form-select");
 const submitBttn = document.getElementById("submit");
 
-const taskList = [];
+export const taskList = [];
 
 class Task {
     constructor(name, description, date, project) {
@@ -50,43 +50,11 @@ function updateTaskList(ev) {
     )
     taskList.push(task);
     document.querySelector('form').reset();
-
-    if (storageAvailable("localStorage")) {
-        localStorage.setItem('task', JSON.stringify(taskList));
-      } else {
-        // Too bad, no localStorage for us
-    }
-
-    const storedTask = localStorage.getItem('task');
-    console.log(JSON.parse(storedTask));
+    
+    addToLocalStorage(taskList);
 }
 
-function storageAvailable(type) {
-    let storage;
-    try {
-      storage = window[type];
-      const x = "__storage_test__";
-      storage.setItem(x, x);
-      storage.removeItem(x);
-      return true;
-    } catch (e) {
-      return (
-        e instanceof DOMException &&
-        // everything except Firefox
-        (e.code === 22 ||
-          // Firefox
-          e.code === 1014 ||
-          // test name field too, because code might not be present
-          // everything except Firefox
-          e.name === "QuotaExceededError" ||
-          // Firefox
-          e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
-        // acknowledge QuotaExceededError only if there's something already stored
-        storage &&
-        storage.length !== 0
-      );
-    }
-  }
+
 
 
 
