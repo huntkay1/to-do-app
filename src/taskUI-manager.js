06e00b31f3ab5e1cd36048fca6e7e508a48490b3
Kayla Hunt <huntkay1@msu.edu>
2024-05-31 17:59:27 -0400

    Remove taskName event listener

--- src/taskUI-manager.js
+++ src/taskUI-manager.js
@@ -146,10 +146,6 @@ export function createTaskElements(taskList, buttonName) {
             const selectedTask = e.currentTarget.closest('.task-item');
             crossOffTask(selectedTask, checkbox);
         });
-        taskName.addEventListener('click', (e) => {
-            const selectedTask = e.currentTarget.closest('.task-item');
-            expandTaskItem(selectedTask, expandIcon);
-        });
         expandIcon.addEventListener('click', (e) => {
             const selectedTask = e.currentTarget.closest('.task-item');
             expandTaskItem(selectedTask, expandIcon, taskContainer, task);
@@ -165,7 +161,7 @@ function expandTaskItem(selectedTask, expandIcon, taskContainer, task) {
     if (inEditMode) {
         exitEditMode(taskContainer, task, selectedTask)
     };
-    
+
     if (isExpanded) {
         expandIcon.innerHTML = '<svg fill="#282828" height="15px" width="15px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xml:space="preserve"> <path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394 l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"/></svg>'
         taskDetails.style.display = 'none';
