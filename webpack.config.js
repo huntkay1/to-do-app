f65de6051592ec0520dffacf94514091e2fb9159
Kayla Hunt <huntkay1@msu.edu>
2024-05-31 18:03:32 -0400

    Add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,22 @@ module.exports = {
             type: 'asset/resource',
           },
         ] 
+      },
+      module: {
+        rules: [
+          {
+            test: /\.(?:js|mjs|cjs)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { targets: "defaults" }]
+                ]
+              }
+            }
+          }
+        ]
       }
       
 };
